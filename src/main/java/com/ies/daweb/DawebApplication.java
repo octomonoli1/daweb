0b94cd3fa97bc9efcab305e4fca53bd545a8a086
Emilio153 <tukaali@hotmail.com>
2025-10-22 09:38:21 +0200

    Rama Vanesa y Emilio

--- src/main/java/com/ies/daweb/DawebApplication.java
+++ src/main/java/com/ies/daweb/DawebApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DawebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DawebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DawebApplication.class, args);
+    }
+
 
 }
