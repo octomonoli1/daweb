2fddaca5872f0163cba0f4583e0a6969e4eaffd4
speicon1911 <salvadorpeinado111906@gmail.com>
2025-10-24 17:33:22 +0200

    Merge y creacion de findByNombre
    Se ha realizado el merge de las ramas de Miguel y Raquel, y de emilio y vanesa, ademas se ha creado en repository un findByName y en servicio y en controller los correspondientes usos

--- src/main/java/com/ies/daweb/web/controllers/AlumnoController.java
+++ src/main/java/com/ies/daweb/web/controllers/AlumnoController.java
@@ -4,11 +4,9 @@ import com.ies.daweb.service.AlumnoService;
 import com.ies.daweb.service.exceptions.AlumnoNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/alumno")
