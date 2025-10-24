506df498f461a398aa2b9e8dd084736ac16d51f9
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2025-10-24 13:46:56 +0200

    merge

--- src/main/java/com/ies/daweb/persistence/repositories/AlumnoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ies.daweb.persistence.repositories;
-
-import com.ies.daweb.persistence.entities.Alumno;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface AlumnoRepository extends ListCrudRepository<Alumno,Integer> {
-}
