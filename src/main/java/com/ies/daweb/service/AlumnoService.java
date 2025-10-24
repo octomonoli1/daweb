32385f1d875a319f9f306b7c075c0b18ae0bab13
Emilio153 <tukaali@hotmail.com>
2025-10-24 14:13:37 +0200

    Rama Vanesa y Emilio: Buscar alumno por id

--- src/main/java/com/ies/daweb/service/AlumnoService.java
+++ src/main/java/com/ies/daweb/service/AlumnoService.java
@@ -51,12 +51,7 @@ public class AlumnoService {
 
         return alumnoRepository.save(alumno);
     }
-    public Alumno findById(int id){
-        if(!this.alumnoRepository.existsById(id)){
-            throw new AlumnoNotFoundException("El id " + id + "no pertenece a ningun alumno");
-        }
-        return this.alumnoRepository.findById(id).get();
-    }
+
 
     public Alumno findById(int id){
         if(!this.alumnoRepository.existsById(id)){
