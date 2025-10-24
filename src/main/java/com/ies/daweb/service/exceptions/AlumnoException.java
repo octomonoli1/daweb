b89ccb079af53fc4be9fd4a9ff170d1355d0297a
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2025-10-24 13:46:56 +0200

    merge

--- src/main/java/com/ies/daweb/service/exceptions/AlumnoException.java
+++
@@ -1,9 +0,0 @@
-package com.ies.daweb.service.exceptions;
-
-public class AlumnoException extends RuntimeException{
-    static final long serialVersionUID = 1L;
-
-    public AlumnoException(String message) {
-        super(message);
-    }
-}
