51bbdb38a31e834282950b71ac2333eb25f22b73
Franflii <franlopezpineda@gmail.com>
2025-10-24 17:04:53 +0200

    createAlumno (24/10)

---
+++ src/main/java/com/ies/daweb/service/exceptions/AlumnoException.java
@@ -0,0 +1,9 @@
+package com.ies.daweb.service.exceptions;
+
+public class AlumnoException extends RuntimeException{
+    static final long serialVersionUID = 1L;
+
+    public AlumnoException(String message) {
+        super(message);
+    }
+}
