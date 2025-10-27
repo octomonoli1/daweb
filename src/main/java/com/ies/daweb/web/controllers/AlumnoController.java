554af58138968844af4cb6fdbdecb8a5b65676b3
jessuss00 <jesusmadro23@gmail.com>
2025-10-27 08:47:55 +0100

    controller

--- src/main/java/com/ies/daweb/web/controllers/AlumnoController.java
+++ src/main/java/com/ies/daweb/web/controllers/AlumnoController.java
@@ -1,14 +1,12 @@
 package com.ies.daweb.web.controllers;
 
+import com.ies.daweb.persistence.entities.Alumno;
 import com.ies.daweb.service.AlumnoService;
 import com.ies.daweb.service.exceptions.AlumnoNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/alumno")
@@ -34,4 +32,22 @@ public class AlumnoController {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
             }
         }
+
+
+        @PutMapping("/{id}")
+        public ResponseEntity<?> update(@PathVariable int id, @RequestBody Alumno alumno) {
+            try {
+                return ResponseEntity.ok(this.alumnoService.update(alumno, id));
+            } catch (AlumnoNotFoundException ex) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+            } catch (AlumnoNotFoundException ex) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+            }
+        }
+
+
+
+
+
+
 }
